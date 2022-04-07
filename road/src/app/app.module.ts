e3e34aec5f78e2229a3c2a39b8c6aa9d1d1129ce
Emily Grace Lester <emhagerman5678@gmail.com>
2022-04-07 14:52:44 -0500

    more nav bar and linking new child

--- road/src/app/app.module.ts
+++ road/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SheltersComponent } from './shelters/shelters.component';
+import { PostsComponent } from './shelters/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SheltersComponent
+    SheltersComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
